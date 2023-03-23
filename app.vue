4ade496dfe12df91efed17f2239b5011e19483f0
mnik01 <maksimgrinberg01@gmail.com>
2023-03-24 00:55:24 +0500

    wip

--- app.vue
+++ app.vue
@@ -1,25 +1,5 @@
 <template>
   <div class="h-fit w-screen overflow-hidden">
-    <ContentNavigation v-slot="{ navigation }">
-      <ul>
-        <li v-for="link of navigation" :key="link._path">
-          <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
-        </li>
-      </ul>
-    </ContentNavigation>
-    <div>
-      <h1>Color mode: {{ $colorMode.value }}</h1>
-      <select v-model="$colorMode.preference">
-        <option value="system">System</option>
-        <option value="light">Light</option>
-        <option value="dark">Dark</option>
-        <option value="sepia">Sepia</option>
-      </select>
-    </div>
     <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts">
-const colorMode = useColorMode();
-</script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,1 +1,23 @@
-<template>lol</template>
+<template>
+  <div class="bg-white shadow-lg w-full">header</div>
+  <!-- <ContentNavigation v-slot="{ navigation }">
+    <ul>
+      <li v-for="link of navigation" :key="link._path">
+        <NuxtLink :to="link._path">{{ link.title }}</NuxtLink>
+      </li>
+    </ul>
+  </ContentNavigation> -->
+  <!-- <div>
+      <h1>Color mode: {{ $colorMode.value }}</h1>
+      <select v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+        <option value="sepia">Sepia</option>
+      </select>
+    </div> -->
+</template>
+
+<!-- <script setup lang="ts">
+const colorMode = useColorMode();
+</script> -->
